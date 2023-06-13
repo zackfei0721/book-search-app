da8691724c44d299da84e3838e280a159612ae02
zackfei0721 <zackfei0721@gmail.com>
2023-06-13 01:23:05 -0400

    fixed WishList

--- src/components/WishList.tsx
+++ src/components/WishList.tsx
@@ -16,20 +16,24 @@ const Wishlist: React.FC = () => {
     <div>
       <h1>My Wishlist</h1>
       {wishlist.length > 0 ? (
-        wishlist.map((book: Book) => (
+        wishlist.map((book: any, index: number) => (
           <div 
-            key={book.id} 
+            key={index} 
             style={{border: "1px solid black", borderRadius: '5px', padding: "10px", margin: "10px"}}>
-            <p>
-              {book.volumeInfo.title}
-            </p>
-            <img 
-              src={book.volumeInfo.imageLinks?.thumbnail}
-              alt={book.volumeInfo.title} />
-            <p> 
-              {book.volumeInfo.description}
-            </p>
-            <button onClick={() => handleRemoveFromWishlist(book.id)}>Remove from wishlist</button>
+            {book?.volumeInfo && (
+              <>
+                <h3>
+                  {book.volumeInfo.title}
+                </h3>
+                <img 
+                  src={book.volumeInfo.imageLinks?.thumbnail}
+                  alt={book.volumeInfo.title} />
+                <p> 
+                  {book.volumeInfo.description}
+                </p>
+                <button onClick={() => handleRemoveFromWishlist(book.id)}>Remove from wishlist</button>
+              </>
+            )}
           </div>
         ))
       ) : (
