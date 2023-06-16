2fa95f97bb7c92adbc65036c92969d8f3cb7478e
zackfei0721 <zackfei0721@gmail.com>
2023-06-15 22:20:46 -0400

    added auto-drop tab

--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useAppDispatch } from '../redux/store';
 import { addBook } from '../redux/wishlistSlice';
 import Pagination from '@mui/material/Pagination'; // imported MUI for pagination
@@ -22,23 +22,74 @@ const SearchPage: React.FC = () => {
   const [initialTotalResults, setInitialTotalResults] = useState(0); // Prevent total page num from changing when changing page
 
 
+  // Autocomplete
+  const [suggestions, setSuggestions] = useState([]);
+  const [selectedSuggestion, setSelectedSuggestion] = useState(-1);
+  const [autoCompleteBooks, setAutoCompleteBooks] = useState([]); // New state for autocomplete books
+  const [displayAutoComplete, setDisplayAutoComplete] = useState(false); // New state for display of autocomplete
+  const searchRef = useRef<HTMLFormElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
   useEffect(() => {
-    fetchData();
-  }, [currentPage, query]);
+    if (query !== "") fetchAutoCompleteData();
+    else setAutoCompleteBooks([]);
+  }, [query]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (searchRef.current && !searchRef.current.contains(event.target as Node)) {
+        setDisplayAutoComplete(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (currentPage !== 1) fetchData();
+  }, [currentPage]);
 
   const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    console.log(event);
-    setCurrentPage(value);
-  };
+      console.log(event);
+      setCurrentPage(value);
+    };
 
   const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
+    setDisplayAutoComplete(true); // Show autocomplete dropdown when typing
   };
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setDisplayAutoComplete(false); // Hide autocomplete dropdown when submit
     fetchData();
   };
+
+  const fetchAutoCompleteData = async () => {
+    setIsLoading(true);
+    try {
+      const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}&startIndex=0&maxResults=5`;
+      const response = await fetch(url);
+      if (!response.ok) throw new Error('HTTP error ' + response.status);
+      const data = await response.json();
+      if (data.items) {
+        setAutoCompleteBooks(data.items);
+      } else {
+        setAutoCompleteBooks([]);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError(String(error));
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
 
   const fetchData = async () => {
     if (!query) {
@@ -107,20 +158,45 @@ const SearchPage: React.FC = () => {
   if (error) return <div>{error}</div>;
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', }}>
-      <h2 style={{textAlign: 'center'}}>Search Page</h2>
-      <form style={{textAlign: 'center'}}
-            onSubmit={handleSearch}>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <h2 style={{ textAlign: 'center' }}>Search Page</h2>
+    <div style={{ position: 'relative', textAlign: 'center' }}>
+      <form onSubmit={handleSearch} ref={searchRef}>
         <input 
-            type='text' 
-            value={query} 
-            onChange={handleQueryChange} />
-        <button 
-            style={{marginLeft: "10px", borderRadius: '5px'}}
-            type='submit'>
-              Search
-        </button>
+          type='text' 
+          value={query} 
+          ref={inputRef}
+          onChange={handleQueryChange} 
+        />
+        <button style={{ marginLeft: "10px", borderRadius: '5px' }} type='submit'>Search</button>
+        {displayAutoComplete && autoCompleteBooks.length > 0 && (
+          <div 
+            style={{ position: "relative", 
+                     backgroundColor: 'white', 
+                     zIndex: 1, 
+                     width: '100%',
+                     left: '50%',
+                     transform: 'translateX(-50%)' }}>
+            {autoCompleteBooks.map((book: any, index: number) => (
+              <div
+                key={index}
+                onClick={() => {
+                  setQuery(book.volumeInfo.title);
+                  setDisplayAutoComplete(false);
+                }}
+                style={{
+                  background: index === selectedSuggestion ? "#ddd" : undefined,
+                  padding: "10px",
+                  cursor: "pointer",
+                }}
+              >
+                {book.volumeInfo.title}
+              </div>
+            ))}
+          </div>
+        )}
       </form>
+    </div>
       {books.map((book: any, index: number) => (
         <div key={index} style={{border: "1px solid black", borderRadius: '5px' ,padding: "10px", margin: "10px"}}>
           {addedBookId === book.id && <p style={{color: "green"}}>{successMessage}</p>}
